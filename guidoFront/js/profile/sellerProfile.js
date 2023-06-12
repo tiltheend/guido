0565cc5660ec33fdb29ec5bfedac767ffe80c985
Nabong <99lnh@naver.com>
2023-06-12 17:49:59 +0900

    230612_18시 나현 판매자 프로필 + 답글 화면 구현 완~~

--- guidoFront/js/profile/sellerProfile.js
+++ guidoFront/js/profile/sellerProfile.js
@@ -4,3 +4,9 @@ const profileEditIcon = document.querySelector(".profile-edit-icon");
 if(profileEditIcon!=null){
     profileEditIcon.addEventListener('click', () => userProfileFile.click());
 }
+
+/* 댓글 눌렀을 때 상세 페이지 */
+const commentView = document.querySelector(".comment-view");
+commentView.addEventListener("click",()=>{
+    location.href="sellerReviewMore.html"
+})
