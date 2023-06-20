126a7d709d715ed19d175654d9eaefb93ee451c7
kimyoji <bluebirdxday@naver.com>
2023-06-20 21:03:50 +0900

    controller, model 추가

--- guido/src/main/java/com/guido/reservation/model/service/ReservationServiceImpl.java
+++ guido/src/main/java/com/guido/reservation/model/service/ReservationServiceImpl.java
@@ -1,8 +1,13 @@
 package com.guido.reservation.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.guido.reservation.model.dao.ReservationMapper;
 
 @Service
 public class ReservationServiceImpl implements ReservationService{
-
+	
+	@Autowired
+	private ReservationMapper mapper;
 }
