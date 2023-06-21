d91d21f3e93e061b1482cc5fdfd0275f1387b3ac
Leenahyeon <99lnh@naver.com>
2023-06-21 16:50:43 +0900

    나현 수정

--- guido/src/main/java/com/guido/profile/controller/ProfileController.java
+++ guido/src/main/java/com/guido/profile/controller/ProfileController.java
@@ -1,5 +1,17 @@
 package com.guido.profile.controller;
 
-public class ProfileController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/profile")
+@Controller
+public class ProfileController {
+	
+	@GetMapping("/mypage")
+	public String mypage() {
+		return "profile/buyerProfile";
+//		return "profile/sellerProfile";
+	}
+	
 }
