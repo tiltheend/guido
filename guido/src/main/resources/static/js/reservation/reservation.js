bb484d2e99825aef43bf5b4178086d018194ce35
kimyoji <bluebirdxday@naver.com>
2023-06-29 17:50:11 +0900

    포트원 API 추가

--- guido/src/main/resources/static/js/reservation/reservation.js
+++ guido/src/main/resources/static/js/reservation/reservation.js
@@ -130,23 +130,20 @@ if (lastYear !== newYear) {
   IMP.init(impCode); 
 
 
+// 결제 요청
+function requestCardPay() {
 
-// // 결제 요청
-// function requestPay() {
+  IMP.request_pay({
+    pg: "kcp.{" + pgMid +"}",     // 상점 ID
+    pay_method: "card",
+    merchant_uid: merchantUid,   // 주문번호
+    name: productName,    // 상품명
+    amount: 64900,                         // 숫자 타입
+    buyer_email: "gildong@gmail.com",
+    buyer_name: "홍길동",
+    buyer_tel: "010-4242-4242",
+  }, function (rsp) { // callback
+    //rsp.imp_uid 값으로 결제 단건조회 API를 호출하여 결제결과를 판단합니다.
 
-//   IMP.request_pay({
-//     pg: "kcp.{상점ID}",
-//     pay_method: "card",
-//     merchant_uid: "ORD20180131-0000011",   // 주문번호
-//     name: "노르웨이 회전 의자",
-//     amount: 64900,                         // 숫자 타입
-//     buyer_email: "gildong@gmail.com",
-//     buyer_name: "홍길동",
-//     buyer_tel: "010-4242-4242",
-//     buyer_addr: "서울특별시 강남구 신사동",
-//     buyer_postcode: "01181"
-//   }, function (rsp) { // callback
-//     //rsp.imp_uid 값으로 결제 단건조회 API를 호출하여 결제결과를 판단합니다.
-
-//   });
-// }
+  });
+}
