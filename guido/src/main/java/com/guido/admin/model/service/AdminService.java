399624262a1ba98d61f09b9ea05e8342a7308e80
DH_NoteBook <poer214@naver.com>
2023-06-30 17:38:47 +0900

    23-06-30 가이드요청승인 완료, 이벤트 컬러피커추가(컨트롤러 수정해야함)

--- guido/src/main/java/com/guido/admin/model/service/AdminService.java
+++ guido/src/main/java/com/guido/admin/model/service/AdminService.java
@@ -10,9 +10,15 @@ import com.guido.common.model.dto.Event;
 import com.guido.common.model.dto.QNA;
 
 public interface AdminService {
-	int eventWrite(Event event, List<MultipartFile> files) throws IllegalStateException, IOException;
+	int writeEvent(Event event, List<MultipartFile> files) throws IllegalStateException, IOException;
 
 	Map<String, Object> selectList(String pageName, int cp);
 
 	QNA selectQNA(int qnaNo);
+
+	int writeAnswer(QNA qna);
+
+	int approveGuide(List<Integer> userNoList);
+
+	List<Event> selectMainEventList();
 }
