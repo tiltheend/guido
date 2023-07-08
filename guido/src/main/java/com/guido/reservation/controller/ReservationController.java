d5040e59c9a9a454bd3b9d8a18030e2a6b0d6f04
bluebirdxday <bluebirdxday@naver.com>
2023-07-08 17:09:27 +0900

    예약 조회 쿼리 수정(옵션 번호 null 값일 때 처리)

--- guido/src/main/java/com/guido/reservation/controller/ReservationController.java
+++ guido/src/main/java/com/guido/reservation/controller/ReservationController.java
@@ -191,9 +191,12 @@ public class ReservationController {
 		map.put("orderNumber", orderNumber);
 		map.put("reservationNo", reservationNo);
 		
+		System.out.println(map);
 		
 		 if(reservationNo!=null)
 			 reservation = service.selectReservation(map);
+		 
+		 System.out.println(reservation);
 		 
 		 if(reservation==null)
 			 return "redirect:/";
